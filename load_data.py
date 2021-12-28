b5668c850c508682581c796f4bc45c46bb8f8d79
Chris Shen <chrshen@wharton.upenn.edu>
2021-12-28 13:06:48 -0500

    12/27/2021

--- load_data.py
+++ load_data.py
@@ -1,46 +1,32 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import numpy as np
-
-# THIS USES THE SPOPTIPY LIBRARY
+import json
 
 def main():
     scope = "user-library-read user-follow-read user-top-read playlist-read-private"
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
-    train_playlist = sp.playlist_items(playlist_id="7CTGQKYCey8L1ZUMQjYxYn")
 
-    length = train_playlist['total']
+    playlists = sp.user_playlists('cshen330', limit=50)
 
-    labels = [ "popularity", "explicit", "duration_ms", "danceability", "energy",
-              "key", "loudness", "mode", "speechiness", "acousticness"]
+    track_ids_and_audio_features = dict()
 
-    train_data = np.ndarray(shape=(length, len(labels)))
-
-    # for i, label in enumerate(labels):
-    #   np.put_along_axis(train_data, np.array([[i]]), label, axis=0)
-
-    while train_playlist:
-        for i, item in enumerate(train_playlist['items']):
-            track = item['track']
-
-            """
-            train_data.put((i, 0), track['popularity'])
-            train_data.put((i, 1), track['explicit'])
-            train_data.put((i, 2), track['duration_ms'])
-            train_data.put((i, 3), features['danceability'])
-            train_data.put((i, 4), track['energy'])
-            train_data.put((i, 5), track['key'])
-            train_data.put((i, 6), track['loudness'])
-            train_data.put((i, 7), track['mode'])
-            train_data.put((i, 8), track['speechiness'])
-            train_data.put((i, 9), track['acousticness'])
-            """
-
-            print("%4d %s %s" % (i + 1 + train_playlist['offset'], track['artists'], track['name']))
-        if train_playlist['next']:
-            train_playlist = sp.next(train_playlist)
+    while playlists:
+        for i, playlist in enumerate(playlists['items']):
+            if playlist['owner']['id'] == 'cshen330':
+                print(playlist['name'])
+                tracks = sp.playlist_items(playlist['id'])
+                for j, track in enumerate(tracks['items']):
+                    if track is not None and track['is_local'] is False:
+                        id = track['track']['id']
+                        audio_features = sp.audio_features(id)
+                        track_ids_and_audio_features.update({id : audio_features})
+        if playlists['next']:
+            playlists = sp.next(playlists)
         else:
-            train_playlist = None
+            playlists = None
+
+    with open('local_data.txt', 'w') as convert_file:
+        convert_file.write(json.dumps(track_ids_and_audio_features))
 
 
 if __name__ == "__main__":
--- load_data_2.py
+++ load_data_2.py
@@ -1,9 +1,9 @@
 import requests
-import pandas as pd
 
 CLIENT_ID = 'a83dd99f76b94da6a263c2a412801031'
 CLIENT_SECRET = 'cdf063e674bc4dee85256cfa6a5db765'
 AUTH_URL = 'https://accounts.spotify.com/api/token'
+USER_ID = 'cshen330'
 
 # PLAYLIST_ID
 PLAYLIST_ID = '7CTGQKYCey8L1ZUMQjYxYn'
@@ -29,16 +29,17 @@ headers = {
     'Authorization': 'Bearer {token}'.format(token=access_token)
 }
 
-# GET Playlist Items
-r = requests.get(BASE_URL + 'playlists/' + PLAYLIST_ID + '/tracks', headers=headers)
+params = {
+    'limit': 50
+}
+
+# GET ALl of my playlists
+r = requests.get(BASE_URL + 'users/' + USER_ID + '/playlists', headers=headers, params=params)
 print(r.status_code)
 
 # convert request to json file
-tracks = r.json()['items']
+r = r.json()
 
-for track in tracks:
-    # get audio features (key, liveness, danceability, ...)
-    f = requests.get(BASE_URL + 'audio-features/' + track['track']['id'],
-                     headers=headers)
-    f = f.json()
-    print(f.keys())
+for playlist in r['items']:
+    print(playlist['name'])
+
